b54ac9a91d493b3bfddf4e507a4e2237c529c777
derfbwh <derfbwh@gmail.com>
2024-08-20 20:53:30 -0400

    Update swing.js

--- forecast/swing.js
+++ forecast/swing.js
@@ -1,13 +1,13 @@
 // Backend-like data storage for swing states with partisan lean included
 let pollData = [
-    { state: 'Florida', electoralVotes: 30, harris: 42.8, trump: 48.0, partisanLean: -.7396878 }, // Leaning Republican
-    { state: 'Pennsylvania', electoralVotes: 19, harris: 46, trump: 44.5, partisanLean: -.296776 }, // Leaning Democratic
-    { state: 'Wisconsin', electoralVotes: 10, harris: 47.4, trump: 44.1, partisanLean: -.3839976 }, // Leaning Democratic
-    { state: 'Michigan', electoralVotes: 15, harris: 46.0, trump: 43.0, partisanLean: -.1175974 }, // Leaning Democratic
-    { state: 'Arizona', electoralVotes: 11, harris: 45.8, trump: 43.7, partisanLean: -.7171309 }, // Leaning Republican
-    { state: 'Georgia', electoralVotes: 16, harris: 45.1, trump: 46.5, partisanLean: -.7357549 }, // Leaning Republican
-    { state: 'Nevada', electoralVotes: 6, harris: 44.7, trump: 44.9, partisanLean: -.259005 },
-    { state: 'North Carolina', electoralVotes: 15, harris: 45.7, trump: 45.6, partisanLean: -.4815506 } // Leaning Republican
+    { state: 'Florida', electoralVotes: 30, harris: 42.8, trump: 47.9, partisanLean: -.7396878 }, // Leaning Republican
+    { state: 'Pennsylvania', electoralVotes: 19, harris: 46.2, trump: 44.8, partisanLean: -.296776 }, // Leaning Democratic
+    { state: 'Wisconsin', electoralVotes: 10, harris: 47.5, trump: 44.1, partisanLean: -.3839976 }, // Leaning Democratic
+    { state: 'Michigan', electoralVotes: 15, harris: 46.5, trump: 43.5, partisanLean: -.1175974 }, // Leaning Democratic
+    { state: 'Arizona', electoralVotes: 11, harris: 45.1, trump: 44.2, partisanLean: -.7171309 }, // Leaning Republican
+    { state: 'Georgia', electoralVotes: 16, harris: 45.2, trump: 46.7, partisanLean: -.7357549 }, // Leaning Republican
+    { state: 'Nevada', electoralVotes: 6, harris: 44.8, trump: 44.3, partisanLean: -.259005 },
+    { state: 'North Carolina', electoralVotes: 15, harris: 45.8, trump: 45.8, partisanLean: -.4815506 } // Leaning Republican
 ]
 
 // Function to fetch the last modified date of the JavaScript file
