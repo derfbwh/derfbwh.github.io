2cd8595fcfcf325302069f896452ef2d7fd0afe2
derfbwh <derfbwh@gmail.com>
2024-08-26 15:29:00 -0400

    Update swing.js

--- forecast/swing.js
+++ forecast/swing.js
@@ -1,13 +1,13 @@
 // Backend-like data storage for swing states with partisan lean included
 let pollData = [
-    { state: 'Florida', electoralVotes: 30, harris: 44.1, trump: 48.7, partisanLean: -.7396878 }, // Leaning Republican
-    { state: 'Pennsylvania', electoralVotes: 19, harris: 47.7, trump: 46, partisanLean: -.296776 }, // Leaning Democratic
-    { state: 'Wisconsin', electoralVotes: 10, harris: 48.9, trump: 45.5, partisanLean: -.3839976 }, // Leaning Democratic
-    { state: 'Michigan', electoralVotes: 15, harris: 48, trump: 44.6, partisanLean: -.1175974 }, // Leaning Democratic
-    { state: 'Arizona', electoralVotes: 11, harris: 46.6, trump: 45.1, partisanLean: -.7171309 }, // Leaning Republican
-    { state: 'Georgia', electoralVotes: 16, harris: 46.8, trump: 47.5, partisanLean: -.7357549 }, // Leaning Republican
-    { state: 'Nevada', electoralVotes: 6, harris: 46.2, trump: 45, partisanLean: -.259005 },
-    { state: 'North Carolina', electoralVotes: 15, harris: 46.8, trump: 46.5, partisanLean: -.4815506 } // Leaning Republican
+    { state: 'Florida', electoralVotes: 30, harris: 44.1, trump: 48.6, partisanLean: -.7396878 },
+    { state: 'Pennsylvania', electoralVotes: 19, harris: 47.7, trump: 46, partisanLean: -.296776 },
+    { state: 'Wisconsin', electoralVotes: 10, harris: 48.9, trump: 45.5, partisanLean: -.3839976 },
+    { state: 'Michigan', electoralVotes: 15, harris: 47.9, trump: 44.8, partisanLean: -.1175974 },
+    { state: 'Arizona', electoralVotes: 11, harris: 46.6, trump: 45.1, partisanLean: -.7171309 },
+    { state: 'Georgia', electoralVotes: 16, harris: 46.8, trump: 47.5, partisanLean: -.7357549 },
+    { state: 'Nevada', electoralVotes: 6, harris: 46.2, trump: 44.9, partisanLean: -.259005 },
+    { state: 'North Carolina', electoralVotes: 15, harris: 46.9, trump: 46.5, partisanLean: -.4815506 }
 ]
 
 // Function to fetch the last modified date of the JavaScript file
