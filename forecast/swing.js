8f6243a40aaeab5ef94f126de3e409dec65c5bac
derfbwh <derfbwh@gmail.com>
2024-09-22 13:21:51 -0400

    swing

--- forecast/swing.js
+++ forecast/swing.js
@@ -1,9 +1,9 @@
 // Backend-like data storage for swing states with partisan lean included
 let pollData = [
-    { state: 'Florida', electoralVotes: 30, harris: 46.6, trump: 50.4, partisanLean: -.7396878 },
+    { state: 'Florida', electoralVotes: 30, harris: 46.6, trump: 50.3, partisanLean: -.7396878 },
     { state: 'Pennsylvania', electoralVotes: 19, harris: 49.3, trump: 47.5, partisanLean: -.296776 },
     { state: 'Wisconsin', electoralVotes: 10, harris: 49.6, trump: 47.8, partisanLean: -.3839976 },
-    { state: 'Michigan', electoralVotes: 15, harris: 49.9, trump: 47.1, partisanLean: -.1175974 },
+    { state: 'Michigan', electoralVotes: 15, harris: 50, trump: 47.1, partisanLean: -.1175974 },
     { state: 'Arizona', electoralVotes: 11, harris: 48, trump: 48, partisanLean: -.7171309 },
     { state: 'Georgia', electoralVotes: 16, harris: 47.3, trump: 48.5, partisanLean: -.7357549 },
     { state: 'Nevada', electoralVotes: 6, harris: 49.1, trump: 47.6, partisanLean: -.259005 },
