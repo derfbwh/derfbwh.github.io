b465380f7d0a6784b2a8622a0dd65f8c78cae1e3
derfbwh <derfbwh@gmail.com>
2024-10-27 17:10:02 -0400

    Update swing.js

--- forecast/swing.js
+++ forecast/swing.js
@@ -1,13 +1,13 @@
 // Backend-like data storage for swing states with partisan lean included
 let pollData = [
-    { state: 'Florida', electoralVotes: 30, harris: 44.5, trump: 51.7, partisanLean: -.7396878 },
-    { state: 'Pennsylvania', electoralVotes: 19, harris: 47.9, trump: 48.6, partisanLean: -.296776 },
-    { state: 'Wisconsin', electoralVotes: 10, harris: 48.5, trump: 48.1, partisanLean: -.3839976 },
-    { state: 'Michigan', electoralVotes: 15, harris: 48.4, trump: 47.7, partisanLean: -.1175974 },
-    { state: 'Arizona', electoralVotes: 11, harris: 47.5, trump: 49.3, partisanLean: -.7171309 },
-    { state: 'Georgia', electoralVotes: 16, harris: 47.3, trump: 49.3, partisanLean: -.7357549 },
-    { state: 'Nevada', electoralVotes: 6, harris: 47.8, trump: 47.9, partisanLean: -.259005 },
-    { state: 'North Carolina', electoralVotes: 15, harris: 47.7, trump: 48.5, partisanLean: -.4815506 }
+    { state: 'Florida', electoralVotes: 30, harris: 44.8, trump: 51.9, partisanLean: -.7396878 },
+    { state: 'Pennsylvania', electoralVotes: 19, harris: 48.2, trump: 48.9, partisanLean: -.296776 },
+    { state: 'Wisconsin', electoralVotes: 10, harris: 48.7, trump: 48.4, partisanLean: -.3839976 },
+    { state: 'Michigan', electoralVotes: 15, harris: 48.7, trump: 47.9, partisanLean: -.1175974 },
+    { state: 'Arizona', electoralVotes: 11, harris: 47.8, trump: 49.6, partisanLean: -.7171309 },
+    { state: 'Georgia', electoralVotes: 16, harris: 47.6, trump: 49.5, partisanLean: -.7357549 },
+    { state: 'Nevada', electoralVotes: 6, harris: 48, trump: 48.2, partisanLean: -.259005 },
+    { state: 'North Carolina', electoralVotes: 15, harris: 48, trump: 48.8, partisanLean: -.4815506 }
 ]
 
 // Function to fetch the last modified date of the JavaScript file
